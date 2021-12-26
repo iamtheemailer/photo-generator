0b9d4ace494f8509d2aa30664a73ef84b6a88e94
unknown <iamtheemailer@gmail.com>
2021-12-26 17:29:44 +0530

    initial commit

---
+++ kee.js
@@ -0,0 +1,7 @@
+
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src="https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg"
+    div.appendChild(image);
+}
